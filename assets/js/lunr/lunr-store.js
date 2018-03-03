edb38211784395de76a171f99019d83507f4009b
hejtmy <hejtmy@gmail.com>
2018-03-03 10:46:20 +0000

    Updating to hejtmy/neuro-coder@de8c67ddca7936e79b2ed5658174fabfe644adef.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,15 +2,15 @@ var store = [{
         "title": "Reading personality traits from between the lines with IBM Watson",
         "excerpt":"In March I went to a talk by one of my colleagues at the Czech NIMH. He is a computer science major PhD student currently working for the IBM and focusing on games, emotions and cognitive sciences overall. He was giving a talk about IBM platform and how it can...","categories": ["Psychology"],
         "tags": ["R","machine-learning","bluemix","IBM"],
-        "url": "/psychology/R-watson-personality/",
+        "url": "http://neuro-coder.com/psychology/R-watson-personality/",
         "teaser":null},{
         "title": "What I would like to have known when I started coding",
         "excerpt":"PROGRAMMING BASICSOnce at a high school, I decided to demonstrate to students, why they should use R instead of Excel for their psychology statistics, and got some of them intrigued. So when I planned my first lesson, oblivious to what experience and approach I can expect, I started to wonder....","categories": ["Coding"],
         "tags": ["teaching","R","programming","coding","personal"],
-        "url": "/coding/intro-to-programming/",
+        "url": "http://neuro-coder.com/coding/intro-to-programming/",
         "teaser":null},{
         "title": "Python explains what is it like to be a graduate student in neuroscience",
         "excerpt":"This is how neuroscience feels in python.from enum import Enum#got his function from supervisor, no idea what it doesfrom god import random_magicclass DataType(Enum): CRAP = 1 WHAT = 2 EFFORT = 3class DataClass: def _init_(self, path): self.data = import_data(path) self.assign_type() def assign_type(self): self.type = random_magic(self.data) def provide_results(data): if(data.type == DataType.CRAP):...","categories": ["PhD"],
         "tags": ["phd","neuroscience","python","fun"],
-        "url": "/phd/neuroscience-phd-python/",
+        "url": "http://neuro-coder.com/phd/neuroscience-phd-python/",
         "teaser":null}]

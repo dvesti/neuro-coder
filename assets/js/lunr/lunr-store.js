988551fcbb92c35394959883e96153b461726b06
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-19 23:25:57 +0000

    Deploy hejtmy/neuro-coder to github.com/hejtmy/neuro-coder.git:gh-pages

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,31 +1,37 @@
 var store = [{
         "title": "Reading personality traits from between the lines with IBM Watson",
-        "excerpt":"In March I went to a talk by one of my colleagues at the Czech NIMH. He is a computer science major PhD student currently working for the IBM and focusing on games, emotions and cognitive sciences overall. He was giving a talk about IBM platform and how it can...","categories": ["Psychology"],
+        "excerpt":"In March I went to a talk by one of my colleagues at the Czech NIMH. He is a computer science major PhD student currently working for the IBM and focusing on games, emotions and cognitive sciences overall. He was giving a talk about IBM platform and how it can...","categories": ["data-science"],
         "tags": ["R","machine-learning","bluemix","IBM"],
-        "url": "http://neuro-coder.com/psychology/R-watson-personality/",
-        "teaser":null},{
+        "url": "http://neuro-coder.com/data-science/R-watson-personality/",
+        "teaser": null
+      },{
         "title": "What I would like to have known when I started coding",
-        "excerpt":"PROGRAMMING BASICS Once at a high school, I decided to demonstrate to students, why they should use R instead of Excel for their psychology statistics, and got some of them intrigued. So when I planned my first lesson, oblivious to what experience and approach I can expect, I started to...","categories": ["Coding"],
-        "tags": ["teaching","R","programming","coding","personal"],
-        "url": "http://neuro-coder.com/coding/intro-to-programming/",
-        "teaser":null},{
+        "excerpt":"PROGRAMMING BASICS Once at a high school, I decided to demonstrate to students, why they should use R instead of Excel for their psychology statistics, and got some of them intrigued. So when I planned my first lesson, oblivious to what experience and approach I can expect, I started to...","categories": ["personal"],
+        "tags": ["teaching","personal","programming","coding"],
+        "url": "http://neuro-coder.com/personal/intro-to-programming/",
+        "teaser": null
+      },{
         "title": "Python explains what it is like to be a graduate student in neuroscience",
-        "excerpt":"This is how neuroscience feels in python. from enum import Enum #got his function from supervisor, no idea what it does from god import random_magic class DataType(Enum): CRAP = 1 WHAT = 2 EFFORT = 3 class DataClass: def _init_(self, path): self.data = import_data(path) self.assign_type() def assign_type(self): self.type = random_magic(self.data)...","categories": ["PhD"],
+        "excerpt":"This is how neuroscience feels in python. from enum import Enum #got his function from supervisor, no idea what it does from god import random_magic class DataType(Enum): CRAP = 1 WHAT = 2 EFFORT = 3 class DataClass: def _init_(self, path): self.data = import_data(path) self.assign_type() def assign_type(self): self.type = random_magic(self.data)...","categories": ["personal"],
         "tags": ["phd","neuroscience","python","fun"],
-        "url": "http://neuro-coder.com/phd/neuroscience-phd-python/",
-        "teaser":null},{
+        "url": "http://neuro-coder.com/personal/neuroscience-phd-python/",
+        "teaser": null
+      },{
         "title": "How to send information from Unity to a web browser - part I.",
-        "excerpt":"When you’are administering expeirments, it is often challenging to keep track of what the participants are doing. You can peak from behind their shoulders, which makes them uneasy, or you can ask them, which ruins your experiment. I often wished, if only there was a way to monitor participants performance...","categories": ["gamedev"],
-        "tags": ["unity","node","science"],
+        "excerpt":"When you’are administering expeirments, it is often challenging to keep track of what the participants are doing. You can peak from behind their shoulders, which makes them uneasy, or you can ask them, which ruins your experiment. I often wished, if only there was a way to monitor participants performance...","categories": ["Gamedev"],
+        "tags": ["unity","node","javascript"],
         "url": "http://neuro-coder.com/gamedev/sending-information-unity-browser/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "How to run anaconda environment in Visual Studio Code terminal",
-        "excerpt":"I love Visual Studio Code. I tried Atom, I tried Sublime, I use Notepadd++ for text editing, but for javascript coding, markdown, blogging and occasionally python coding I use VSC. Atom always seemed sluggish, despite all my effort and even PC upgrade, and sublime was nice and fast, but it...","categories": ["coding"],
-        "tags": ["python","anaconda","visual-studio-code","coding-tips"],
-        "url": "http://neuro-coder.com/coding/visual-studio-code-anaconda/",
-        "teaser":null},{
+        "excerpt":"I love Visual Studio Code. I tried Atom, I tried Sublime, I use Notepadd++ for text editing, but for javascript coding, markdown, blogging and occasionally python coding I use VSC. Atom always seemed sluggish, despite all my effort and even PC upgrade, and sublime was nice and fast, but it...","categories": ["programming"],
+        "tags": ["python","anaconda","visual-studio-code","tips"],
+        "url": "http://neuro-coder.com/programming/visual-studio-code-anaconda/",
+        "teaser": null
+      },{
         "title": "How to run jekyll on windows",
-        "excerpt":"Jekyll is a static page generator which this blogs run on. It basically takes markdown files, html templates and few css and javascript files and then convert them all into a static html website. So every new article you add, every change rebuilds the page completely. Unlike in Wordpress, there...","categories": ["coding","jekyll","ruby"],
-        "tags": ["jekyll","ruby","windows","chocolately","programming","coding","personal"],
-        "url": "http://neuro-coder.com/coding/jekyll/ruby/how-to-run-jekyll-on-windows/",
-        "teaser":null}]
+        "excerpt":"Jekyll is a static page generator which this blogs run on. It basically takes markdown files, html templates and few css and javascript files and then convert them all into a static html website. So every new article you add, every change rebuilds the page completely. Unlike in Wordpress, there...","categories": ["programming"],
+        "tags": ["jekyll","ruby","windows","chocolately","tips"],
+        "url": "http://neuro-coder.com/programming/how-to-run-jekyll-on-windows/",
+        "teaser": null
+      }]
